586c48769150fae0c432fe195c6b9a81570e27bd
matu.dev <mati.a.gimenez2002@gmail.com>
2025-11-26 20:47:48 -0300

    Clase 31

--- Clase-31-React/src/Components/Contador/Contador.jsx
+++ Clase-31-React/src/Components/Contador/Contador.jsx
@@ -9,4 +9,11 @@ No se podra incrementar a mas de 10
 <button>Decrementar</button>
 <span>{contador}</span>
 <button>Incrementar</button>
+
+(Opcional):
+- Agregar mensajes de advertencia: 
+    - "Has llegado al limite"
+    - "No puedes tener menos de 1"
+- Agregar deshabilitaciones a los botones, ejemplo si el contador vale 1 deberia estar deshabilitado el boton de decrementar (o que directamente no se renderize)
+- Que el componente reciba min, max, initial por props y configuren desde que rango puede operar el contador
 */
