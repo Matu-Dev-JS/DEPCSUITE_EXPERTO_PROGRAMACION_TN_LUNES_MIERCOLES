e159cf53a2473bfe3ee6a64353799790fb8213e3
matu.dev <mati.a.gimenez2002@gmail.com>
2025-11-26 18:50:46 -0300

    Clase 31

---
+++ Clase-31-React/src/Components/ParrafoExtendible/ParrafoExtendible.jsx
@@ -0,0 +1,8 @@
+
+function ParrafoExtendible (){
+    return (
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis minima delectus error nobis, ipsa eos obcaecati impedit maxime, ipsum fugiat consequatur accusamus dolores consectetur at quod accusantium est iste amet!</p>
+    )
+}
+
+export default ParrafoExtendible
