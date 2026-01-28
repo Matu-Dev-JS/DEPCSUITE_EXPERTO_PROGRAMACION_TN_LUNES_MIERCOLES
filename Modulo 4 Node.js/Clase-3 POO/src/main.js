9941dc04fba89187145b9053c6b88bc846105a00
matu.dev <mati.a.gimenez2002@gmail.com>
2026-01-28 20:03:42 -0300

    Modulo 4 clase 3 POO

--- Modulo 4 Node.js/Clase-3 POO/src/main.js
+++ Modulo 4 Node.js/Clase-3 POO/src/main.js
@@ -27,18 +27,28 @@ let persona_2 = {nombre: 'pepe', edad: 30}
 This toma un valor distinto dependiendo el contexto en el que es llamado, en el contexto de funcion constructora this es una referencia al objeto que se creara a partir de la funcion
 Las funciones constructoras deben ser definidas con la primera letra mayuscula
 */
-function Persona (nombre, edad){
+/* function Persona (nombre, edad){
     this.nombre = nombre
     this.edad = edad
-}
+    
+} */
+
+
+//Metodo
+//Es una funcioncionalidad sobre la definicion de persona, esto nos asegura que solo las Personas tengan acceso a presentacion
+/* Persona.prototype.presentacion = function (){
+    console.log('Hola me llamo ' + this.nombre + ' y tengo una edad de ' + this.edad)
+} */
+
 
 //Para ejecutar la funcion constructora y "CREAR" una persona debemos usar la palabra reservada new
 //Este proceso tecnicamente se lo conoce como instanciar
-const persona_1 = new Persona('pepe', 30)
-const persona_2 = new Persona('juan', 30)
+/* const persona_1 = new Persona('pepe', 30)
+persona_1.nombre = 'pepesito'
+const persona_2 = new Persona('juan', 30) */
 
-console.log(persona_1)
-
+//Asi se invoca un metodo
+/* persona_1.presentacion() */
 /* 
 Definir la funcion constructora para 
 
@@ -48,11 +58,71 @@ Personaje
     - edad
     - id
     - nivel: 0
+    - revisarVida() Dependiendo de si la vida es menor a 10 dira 'Necesito curarme ya mismo' pero si es menor a 50 dira 'debo tomar un descanso pronto' y si es mayor a 50 dira 'Estoy de lujo'
+    - presentarse() Dira por consola 'Yo soy {nombre} y tengo {edad} años'
+
 
 Item
     - titulo
     - nivel
     - descripcion
+    - describir() Relatara por consola como se ve el objeto
+
+    y hagan 3 ejemplos de cada uno (Y muestren por consola para verificar)
+*/
+
+/* function Personaje ( nombre, edad, id){
+    this.vida= 200;
+    this.nombre= nombre;
+    this.edad= edad;
+    this.id= id;
+    this.nivel= 0;
+}
+function Item (titulo, nivel, descripcion){
+    this.titulo= titulo;
+    this.nivel= nivel;
+    this.descripcion= descripcion;
+}
+const personaje_1= new Personaje('Adriel', 23, 1);
+const personaje_2= new Personaje('Lucia', 22, 2);
+const personaje_3= new Personaje('Carlos', 25, 3);
+const item_1= new Item('Espada', 1, 'Espada de acero');
+const item_2= new Item('Escudo', 1, 'Escudo de madera');
+const item_3= new Item('Pocion', 1, 'Pocion de vida');
+ */
+
+class Persona {
+    constructor (nombre, edad){
+        this.nombre = nombre;
+        this.edad = edad;
+        this.estaVivo = true
+    }
+    presentarse(){
+        console.log('Hola me llamo ' + this.nombre + ' y tengo una edad de ' + this.edad)
+    }
+}
+
+const persona_1 = new Persona('pepe', 30)
+persona_1.presentarse()
+
+
+/* 
+Definir las siguientes clases
+Personaje 
+    - vida: 200
+    - nombre
+    - edad
+    - id
+    - nivel: 0
+    - revisarVida() Dependiendo de si la vida es menor a 10 dira 'Necesito curarme ya mismo' pero si es menor a 50 dira 'debo tomar un descanso pronto' y si es mayor a 50 dira 'Estoy de lujo'
+    - presentarse() Dira por consola 'Yo soy {nombre} y tengo {edad} años'
+
+
+Item
+    - titulo
+    - nivel
+    - descripcion
+    - describir() Relatara por consola como se ve el objeto
 
     y hagan 3 ejemplos de cada uno (Y muestren por consola para verificar)
 */
