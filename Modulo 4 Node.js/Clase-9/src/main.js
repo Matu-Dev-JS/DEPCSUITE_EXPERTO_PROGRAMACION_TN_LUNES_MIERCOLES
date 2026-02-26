f7dbb9daafa181b35f44ec2b187916eddea13f0e
matu.dev <mati.a.gimenez2002@gmail.com>
2026-02-25 21:09:20 -0300

    Clase 13

--- Modulo 4 Node.js/Clase-9/src/main.js
+++ Modulo 4 Node.js/Clase-9/src/main.js
@@ -1,5 +1,23 @@
 import ENVIRONMENT from "./config/environment.config.js"
 import { checkConnectionDB } from "./config/mysql.config.js"
+import userRepository from "./repositories/user.repository.js"
 
 checkConnectionDB()
 
+/* userRepository.create(
+    {
+        name: 'pepe',
+        password: '123',
+        email: 'pepe@gmail.com',
+        telephone_number: '12',
+        img_profile: null
+    }
+) */
+
+    //userRepository.getById(6)
+
+/*     userRepository.getByEmail('pepe@gmail.com') */
+
+/*     userRepository.updateById(1, {name: 'pepesito', password: '1234'}) */
+
+userRepository.getById(5)
--- Modulo 4 Node.js/Clase-9/src/repositories/user.repository.js
+++ Modulo 4 Node.js/Clase-9/src/repositories/user.repository.js
@@ -12,6 +12,8 @@ Arquitectura hexagonal Clean Code
         - ProductRepository maneja todo lo de productos y categoria_productos
 */
 
+import pool from "../config/mysql.config.js"
+
 
 const USER_TABLE = {
     NAME: 'users',
@@ -28,22 +30,106 @@ const USER_TABLE = {
 
 class UserRepository {
     getAll(){
-
+        //Hacer el SELECT * FROM users pero sin where
     }
 
-    getByEmail(){
-
+    async getByEmail(user_email){
+        
+        const [user] = await pool.execute (
+            `
+            SELECT * FROM ${USER_TABLE.NAME} WHERE ${USER_TABLE.COLUMNS.EMAIL} = ?
+            `,
+            [user_email]
+        )
+        const user_found = user[0]
+        return user_found
     }
 
-    getById(){
+    async getById(user_id){
+        const [users] = await pool.execute(
+            `
+            SELECT * FROM ${USER_TABLE.NAME} WHERE ${USER_TABLE.COLUMNS.ID} = ?
+            `,
+            [user_id]
+        )
 
+        const user_found = users[0]
+        return user_found
     }
 
-    create(){
-
+    async create(
+        {
+            name,
+            password,
+            email, 
+            telephone_number,
+            img_profile
+        }
+    ){
+        const result = await pool.execute(
+            `
+                INSERT INTO ${USER_TABLE.NAME} 
+                (
+                    ${USER_TABLE.COLUMNS.NAME},
+                    ${USER_TABLE.COLUMNS.PASSWORD},
+                    ${USER_TABLE.COLUMNS.EMAIL},
+                    ${USER_TABLE.COLUMNS.TELEPHONE_NUMBER},
+                    ${USER_TABLE.COLUMNS.IMG_PROFILE}
+                )
+                VALUES
+                (
+                    ?,
+                    ?,
+                    ?,
+                    ?,
+                    ?
+                )
+            `,
+            [
+                name, 
+                password, 
+                email, 
+                telephone_number, 
+                img_profile
+            ]
+        )
+        return result[0].insertId || null
     }
 
-    updateById(){
+    //EJEMPLO DE UPDATE EN SQL
+    //Actualiza el nombre y precio del producto con id 8
+    //UPDATE productos SET name = 'tabla rosada', price = 40000 WHERE id = 8 
+
+    async updateById(user_id, change_user){
+        //change_user es un objeto con los nombres de columnas y sus nuevos valores
+        //Ejemplo: change_user = {name: 'pepesito', password: '1234'},  {name: 'pepesito'}
+        //Ustedes tendran que leer el objeto change_user e ir generando la query
+        //La query generada debe ser algo asi: UPDATE users SET name = ?, password = ? WHERE id = ?
+        
+        let query = `UPDATE ${USER_TABLE.NAME} SET `
+
+        const values_to_inject = []
+        const change_user_values = Object.values(change_user) //Transforma el objeto en un array de valores
+        const change_user_properties = Object.keys(change_user) //Transforma el objeto en un array de claves
+       
+        for(const propiedad of change_user_properties){
+            
+            query += `${propiedad} = ?`
+
+            const property_index = change_user_properties.indexOf(propiedad)
+
+            //Mientras no sea el ultimo cambio sumo la coma
+            if(property_index !== change_user_properties.length - 1){
+                query = query + ','
+            }
+
+            let property_value = change_user_values[property_index]
+            values_to_inject.push(property_value)
+        }
+        query += ` WHERE id = ${user_id}`
+        values_to_inject.push(user_id)
+        
+        await pool.execute(query, values_to_inject)
 
     }
     
@@ -55,3 +141,20 @@ class UserRepository {
 const userRepository = new UserRepository()
 
 export default userRepository
+
+/* const changes = {
+    name: 'test',
+    pass: '123'
+}
+let result = ''
+for(let propiedad in changes){
+    const valor_propiedad = changes[propiedad]
+    result = result + `${propiedad}: ${valor_propiedad}\n`
+}
+
+console.log(result) */
+
+/* `
+name: test
+pass: 123
+` */
