b95c09f3c52d80b5476b9046154dd126bcd58ae0
matu.dev <mati.a.gimenez2002@gmail.com>
2026-03-16 18:50:43 -0300

    Clase 14

--- Modulo 4 Node.js/Clase-14 Express/src/main.js
+++ Modulo 4 Node.js/Clase-14 Express/src/main.js
@@ -1,6 +1,23 @@
+import ENVIRONMENT from "./config/environment.config.js";
 import connectMongoDB from "./config/mongoDB.config.js";
-
+import express from 'express'
 
 
 connectMongoDB()
 
+const app = express()
+
+
+app.get('/api/status', (request, response) => {
+    response.status(200).send(
+        {
+            ok: true,
+            message: "Api funcionando correctamente",
+            status: 200
+        }
+    )
+})
+
+app.listen(ENVIRONMENT.PORT, () => {
+    console.log(`El servidor se esta escuchando en el puerto ${ENVIRONMENT.PORT}`)
+})
