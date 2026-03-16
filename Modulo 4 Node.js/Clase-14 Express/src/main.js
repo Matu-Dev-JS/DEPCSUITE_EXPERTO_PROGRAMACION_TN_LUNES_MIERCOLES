0f63d10577b79e68179d45fffac7c3b5bd47bd86
matu.dev <mati.a.gimenez2002@gmail.com>
2026-03-16 19:03:03 -0300

    Clase 14

--- Modulo 4 Node.js/Clase-14 Express/src/main.js
+++ Modulo 4 Node.js/Clase-14 Express/src/main.js
@@ -9,6 +9,20 @@ const app = express()
 
 
 app.get('/api/status', (request, response) => {
+    
+/* 
+las respuestas HTTP tienen un status, el status sirve para marcar rapidamente como fue la respuesta
+Por ejemplo:
+    Si no encuentro un recurso => 404
+    Si todo salio bien => 200
+    Se creo exitosamente => 201
+    Consultaste incorrectamente => 400
+    Desautorizado => 401
+    No podes hacer esa accion (pero estas autentificado) => 403
+    Fallo interno del servidor => 500
+    ..ETC
+*/
+
     response.status(200).send(
         {
             ok: true,
